d9a8c2ee698b3204272728c65c3e223ab56cd9ee
User <lopezs.dev@gmail.com>
2025-06-22 12:03:12 -0600

    refactor: add funcionality to PermissionModel can interact with other classes

--- backend/src/main/java/com/tap/serve/singapur/model/PermissionModel.java
+++ backend/src/main/java/com/tap/serve/singapur/model/PermissionModel.java
@@ -1,4 +1,22 @@
 package com.tap.serve.singapur.model;
 
+import com.tap.serve.singapur.enums.PermissionEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "permissions")
 public class PermissionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "permission_name", nullable = false, updatable = false)
+    private PermissionEnum permissionName;
 }
