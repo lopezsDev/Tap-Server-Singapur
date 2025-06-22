8f67b8293e5e7157b40295849dc8e47d9dd64d02
User <lopezs.dev@gmail.com>
2025-06-22 11:25:57 -0600

    refactor: modify mapping of RolModel class to the BD

--- backend/src/main/java/com/tap/serve/singapur/model/RolModel.java
+++ backend/src/main/java/com/tap/serve/singapur/model/RolModel.java
@@ -1,9 +1,16 @@
 package com.tap.serve.singapur.model;
 
-
 import com.tap.serve.singapur.enums.RolEnum;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "roles")
 public class RolModel {
 
     @Id
